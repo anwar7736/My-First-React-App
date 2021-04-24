03b641aaff6f1af86533d8f176011d6e6ad7e49e
github7736 <anwarhossain7736@gmail.com>
2021-04-24 12:24:31 +0600

    Top Banner

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopBanner from './components/TopBanner';
+
+function App() {
+  return (
+    <div>
+
+     <TopBanner/>
+
+    </div>
+  );
+}
+
+export default App;
