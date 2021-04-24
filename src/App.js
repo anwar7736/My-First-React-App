624b658692b22979a06153bdd98c2a3c8d284646
github7736 <anwarhossain7769@gmail.com>
2021-04-25 00:51:32 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import TopNavigation from './components/TopNavigation';
 import TopBanner from './components/TopBanner';
+import Services from './components/Services';
 
 function App() {
   return (
     <div>
 
+     <TopNavigation/>
      <TopBanner/>
+     <Services/>
 
     </div>
   );
---
+++ src/components/TopNavigation.js
@@ -0,0 +1,51 @@
+import React, {Component, Fragment} from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+import whiteLogo from '../asset/images/navlogo.svg'
+import blueLogo from '../asset/images/navlogoScroll.svg'
+class TopNavigation extends Component{
+	constructor(){
+		super()
+		this.state = {
+			navBarTitle : 'navTitle',
+			navBarLogo  : [whiteLogo],
+			navBarBack  :  'navBackground',
+			navBarItem  : 'navItem'
+
+		}
+	}
+	onScroll=()=>{
+		if(window.scrollY > 100){
+			this.setState({navBarTitle : 'navTitleScroll', navBarLogo : [blueLogo], navBarBack : 'navBackgroundScroll', navBarItem : 'navItemScroll'})
+		}
+		else if(window.scrollY < 100){
+			this.setState({navBarTitle : 'navTitle', navBarLogo : [whiteLogo], navBarBack : 'navBackground', navBarItem : 'navItem'})
+		}
+	}
+	componentDidMount(){
+		window.addEventListener('scroll', this.onScroll)
+	}
+	render(){
+		return(
+			<Fragment>
+				 <Navbar className={this.state.navBarBack} fixed="top" collapseOnSelect expand="lg" variant="dark">
+                    <Navbar.Brand className={this.state.navBarTitle}><img src={this.state.navBarLogo}/> Anwar Hossain</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">SERVICES</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+			</Fragment>
+			);
+	}
+
+}
+export default TopNavigation;
